3c39b1fbf2bc99941adcc44767f26b77dd1829dd
Harvard Pan <harvard@conducehealth.com>
2024-12-09 13:42:20 -0500

    day9 part 2

--- day9/day9.go
+++ day9/day9.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -43,6 +44,7 @@ func calculateResult(disk *list.List) int {
 	index := 0
 	for e := disk.Front(); e != nil; e = e.Next() {
 		if e.Value.(Block).fileId == -1 {
+			index += e.Value.(Block).size
 			continue
 		}
 		for j := 0; j < e.Value.(Block).size; j++ {
@@ -57,9 +59,9 @@ func printDisk(disk *list.List) {
 	fmt.Print("Row: ")
 	for e := disk.Front(); e != nil; e = e.Next() {
 		if e.Value.(Block).fileId == -1 {
-			fmt.Print(".")
+			fmt.Print(strings.Repeat(".", e.Value.(Block).size))
 		} else {
-			fmt.Print(e.Value.(Block).fileId, "")
+			fmt.Print(strings.Repeat(strconv.Itoa(e.Value.(Block).fileId), e.Value.(Block).size))
 		}
 	}
 	fmt.Println()
@@ -67,7 +69,7 @@ func printDisk(disk *list.List) {
 
 func part1() {
 	// https://adventofcode.com/2024/day/9
-	// Walk an operations tree and determine a valid order of operations
+	// defragment the disk and fill in all the space
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -140,6 +142,87 @@ func part1() {
 	fmt.Println("The final result is: ", calculateResult(disk))
 }
 
+func findEmptyBlockWithEnoughSpace(startElement *list.Element, endElement *list.Element, size int) *list.Element {
+	for e := startElement; e != nil; e = e.Next() {
+		if e == endElement {
+			break
+		}
+		if e.Value.(Block).fileId != -1 {
+			continue
+		}
+		if e.Value.(Block).size >= size {
+			return e
+		}
+	}
+	return nil
+}
+
 func part2() {
 	// https://adventofcode.com/2024/day/9#part2
+	// Only defragment file when the entire block can fit
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+	defer timer("part2")()
+	// result := 0
+	// variables specific to this problem
+	fileId := 0 // initial file ID that gets incremented
+	disk := list.New()
+	// Begin file parsing
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		for i, char := range line {
+			size, _ := strconv.Atoi(string(char))
+			if size == 0 {
+				continue
+			}
+			if i%2 == 0 {
+				// file definition
+				disk.PushBack(Block{fileId, size})
+				fileId++
+			} else {
+				// empty space
+				disk.PushBack(Block{-1, size})
+			}
+		}
+		printDisk(disk)
+		break // only one row today
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file: ", err)
+	}
+	fileId = fileId - 1 // last fileId that was used
+	for e := disk.Back(); e != nil; e = e.Prev() {
+		if e.Value.(Block).fileId != fileId {
+			// We go backwards in fileId
+			continue
+		}
+		pInsertPosition := findEmptyBlockWithEnoughSpace(disk.Front(), e, e.Value.(Block).size)
+		if pInsertPosition == nil {
+			// couldn't find any where to put it.
+			fileId--
+			continue
+		}
+		// Need to move e to the insert position, but can only move to the size of the insert position
+		// and then move the rest of the blocks to the next insert position
+		if e.Value.(Block).size < pInsertPosition.Value.(Block).size {
+			// Split up the insert position block.
+			disk.InsertAfter(Block{pInsertPosition.Value.(Block).fileId, pInsertPosition.Value.(Block).size - e.Value.(Block).size}, pInsertPosition)
+			pInsertPosition.Value = Block{e.Value.(Block).fileId, e.Value.(Block).size}
+			e.Value = Block{-1, e.Value.(Block).size}
+		} else {
+			pInsertPosition.Value = e.Value
+			e.Value = Block{-1, e.Value.(Block).size}
+		}
+		fileId--
+		if fileId < 0 {
+			break
+		}
+	}
+	printDisk(disk)
+	fmt.Println("The final result is: ", calculateResult(disk))
 }
