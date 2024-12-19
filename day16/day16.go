958f53a1b521369d7a899afe686684ca9696077c
Harvard Pan <harvard@conducehealth.com>
2024-12-19 12:23:34 -0500

    day16 part 1 - rewritten to use a different data structure

--- day16/day16.go
+++ day16/day16.go
@@ -4,29 +4,32 @@ import (
 	"bufio"
 	"container/heap"
 	"fmt"
+	"math"
 	"os"
 	"time"
 )
 
-type Position struct {
-	x, y      int
-	direction int // degrees, with 0 being north, and 180 being south
+type Node struct {
+	x       int
+	y       int
+	content rune
+	prev    []*PriorityQueueItem // previous node in the path
 }
 
 // Priority Queue using the container/heap package.
 // Took example code from https://pkg.go.dev/container/heap
 
-// An Item is something we manage in a priority queue.
-type Item struct {
-	position     Position // The value of the item; arbitrary.
-	previousItem *Item
-	priority     int // The priority of the item in the queue.
+// An PriorityQueueItem is something we manage in a priority queue.
+type PriorityQueueItem struct {
+	node      *Node
+	direction int // north = 0, east = 90, south = 180, west = 270
+	priority  int // The priority of the item in the queue.
 	// The index is needed by update and is maintained by the heap.Interface methods.
 	index int // The index of the item in the heap.
 }
 
 // A PriorityQueue implements heap.Interface and holds Items.
-type PriorityQueue []*Item
+type PriorityQueue []*PriorityQueueItem
 
 func (pq PriorityQueue) Len() int { return len(pq) }
 
@@ -43,7 +46,7 @@ func (pq PriorityQueue) Swap(i, j int) {
 
 func (pq *PriorityQueue) Push(x any) {
 	n := len(*pq)
-	item := x.(*Item)
+	item := x.(*PriorityQueueItem)
 	item.index = n
 	*pq = append(*pq, item)
 }
@@ -67,14 +70,13 @@ func timer(name string) func() {
 
 func main() {
 	part1()
-	part2()
 }
 
-func printGrid(grid [][]rune, path []*Item) {
+func printGrid(grid [][]*Node, path []*PriorityQueueItem) {
 	visited := make(map[string]string)
 	for _, item := range path {
 		directionString := ""
-		switch item.position.direction {
+		switch item.direction {
 		case 0:
 			directionString = "^"
 		case 90:
@@ -84,43 +86,60 @@ func printGrid(grid [][]rune, path []*Item) {
 		case 270:
 			directionString = "<"
 		}
-		visited[fmt.Sprintf("%d,%d", item.position.x, item.position.y)] = directionString
+		visited[fmt.Sprintf("%d,%d", item.node.x, item.node.y)] = directionString
 	}
 	for rowIndex, row := range grid {
-		for colIndex, char := range row {
+		for colIndex, item := range row {
 			direction, exists := visited[fmt.Sprintf("%d,%d", colIndex, rowIndex)]
 			if exists {
 				fmt.Print(direction)
 			} else {
-				fmt.Print(string(char))
+				fmt.Print(string(item.content))
 			}
 		}
 		fmt.Println()
 	}
 }
 
-func findShortestPath(grid [][]rune, pq PriorityQueue, visited map[string]bool, endPosition Position) []*Item {
+func constructShortestPath(item *PriorityQueueItem) []*PriorityQueueItem {
+	path := make([]*PriorityQueueItem, 0)
+	path = append(path, item)
+	for item != nil && item.priority != 0 {
+		minPriority := math.MaxInt64
+		var minItem *PriorityQueueItem = nil
+		for _, prevItem := range item.node.prev {
+			if prevItem.priority < minPriority {
+				minPriority = prevItem.priority
+				minItem = prevItem
+			}
+		}
+		if minItem == nil {
+			break
+		}
+		path = append(path, minItem)
+		item = minItem
+	}
+	return path
+}
+
+func findShortestPath(grid [][]*Node, pq PriorityQueue, visited map[string]bool, endPosition *Node) []*PriorityQueueItem {
 	for pq.Len() > 0 {
-		item := heap.Pop(&pq).(*Item)
-		if visited[fmt.Sprintf("%d,%d", item.position.x, item.position.y)] {
+		item := heap.Pop(&pq).(*PriorityQueueItem)
+		if visited[fmt.Sprintf("%d,%d,%d", item.node.x, item.node.y, item.direction)] {
 			continue
 		}
-		visited[fmt.Sprintf("%d,%d", item.position.x, item.position.y)] = true
-		if item.position.x == endPosition.x && item.position.y == endPosition.y {
+		visited[fmt.Sprintf("%d,%d,%d", item.node.x, item.node.y, item.direction)] = true
+		if item.node.x == endPosition.x && item.node.y == endPosition.y {
 			fmt.Println("Found the end position")
-			// Build up the path by checking previousItem for each item
-			path := make([]*Item, 0)
-			for item != nil {
-				path = append(path, item)
-				item = item.previousItem
-			}
-			return path
+			// Build up the path by checking prev array for each item's Node
+			// Only add the items in the prev array that have the minimum priority
+			return constructShortestPath(item)
 		}
 		// Add the next possible moves to the queue
 		// Up
-		if item.position.y-1 >= 0 && grid[item.position.y-1][item.position.x] != '#' {
+		if item.node.y-1 >= 0 && grid[item.node.y-1][item.node.x].content != rune('#') {
 			additionalDistance := 0
-			switch item.position.direction {
+			switch item.direction {
 			case 0:
 				additionalDistance = 1
 			case 90, 270:
@@ -128,16 +147,18 @@ func findShortestPath(grid [][]rune, pq PriorityQueue, visited map[string]bool, 
 			case 180:
 				additionalDistance = 2001 // 2000 for 2 turns, 1 for the move
 			}
-			heap.Push(&pq, &Item{
-				position:     Position{x: item.position.x, y: item.position.y - 1, direction: 0},
-				priority:     item.priority + additionalDistance,
-				previousItem: item,
+			node := grid[item.node.y-1][item.node.x]
+			node.prev = append(node.prev, item)
+			heap.Push(&pq, &PriorityQueueItem{
+				node:      node,
+				direction: 0,
+				priority:  item.priority + additionalDistance,
 			})
 		}
 		// Down
-		if item.position.y+1 < len(grid) && grid[item.position.y+1][item.position.x] != '#' {
+		if item.node.y+1 < len(grid) && grid[item.node.y+1][item.node.x].content != rune('#') {
 			additionalDistance := 0
-			switch item.position.direction {
+			switch item.direction {
 			case 180:
 				additionalDistance = 1
 			case 90, 270:
@@ -145,16 +166,18 @@ func findShortestPath(grid [][]rune, pq PriorityQueue, visited map[string]bool, 
 			case 0:
 				additionalDistance = 2001 // 2000 for 2 turns, 1 for the move
 			}
-			heap.Push(&pq, &Item{
-				position:     Position{x: item.position.x, y: item.position.y + 1, direction: 180},
-				priority:     item.priority + additionalDistance,
-				previousItem: item,
+			node := grid[item.node.y+1][item.node.x]
+			node.prev = append(node.prev, item)
+			heap.Push(&pq, &PriorityQueueItem{
+				node:      node,
+				direction: 180,
+				priority:  item.priority + additionalDistance,
 			})
 		}
 		// Left
-		if item.position.x-1 >= 0 && grid[item.position.y][item.position.x-1] != '#' {
+		if item.node.x-1 >= 0 && grid[item.node.y][item.node.x-1].content != rune('#') {
 			additionalDistance := 0
-			switch item.position.direction {
+			switch item.direction {
 			case 270:
 				additionalDistance = 1
 			case 0, 180:
@@ -162,16 +185,18 @@ func findShortestPath(grid [][]rune, pq PriorityQueue, visited map[string]bool, 
 			case 90:
 				additionalDistance = 2001 // 2000 for 2 turns, 1 for the move
 			}
-			heap.Push(&pq, &Item{
-				position:     Position{x: item.position.x - 1, y: item.position.y, direction: 270},
-				priority:     item.priority + additionalDistance,
-				previousItem: item,
+			node := grid[item.node.y][item.node.x-1]
+			node.prev = append(node.prev, item)
+			heap.Push(&pq, &PriorityQueueItem{
+				node:      node,
+				direction: 270,
+				priority:  item.priority + additionalDistance,
 			})
 		}
 		// Right
-		if item.position.x+1 < len(grid[0]) && grid[item.position.y][item.position.x+1] != '#' {
+		if item.node.x+1 < len(grid[0]) && grid[item.node.y][item.node.x+1].content != rune('#') {
 			additionalDistance := 0
-			switch item.position.direction {
+			switch item.direction {
 			case 90:
 				additionalDistance = 1
 			case 0, 180:
@@ -179,20 +204,22 @@ func findShortestPath(grid [][]rune, pq PriorityQueue, visited map[string]bool, 
 			case 270:
 				additionalDistance = 2001 // 2000 for 2 turns, 1 for the move
 			}
-			heap.Push(&pq, &Item{
-				position:     Position{x: item.position.x + 1, y: item.position.y, direction: 90},
-				priority:     item.priority + additionalDistance,
-				previousItem: item,
+			node := grid[item.node.y][item.node.x+1]
+			node.prev = append(node.prev, item)
+			heap.Push(&pq, &PriorityQueueItem{
+				node:      node,
+				direction: 90,
+				priority:  item.priority + additionalDistance,
 			})
 		}
 	}
-	return make([]*Item, 0)
+	return make([]*PriorityQueueItem, 0)
 }
 
 func part1() {
 	// https://adventofcode.com/2024/day/16
-	//
-	file, err := os.Open("input.txt")
+	// Find shortest path through maze - Dijsktra's algorithm
+	file, err := os.Open("sampleinput2.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
@@ -201,22 +228,23 @@ func part1() {
 	defer timer("part1")()
 	result := 0
 	// variables specific to this problem
-	grid := make([][]rune, 0)
-	var startPosition, endPosition Position
+	grid := make([][]*Node, 0)
+	var startPosition, endPosition *Node
 
 	// Begin file parsing
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		row := make([]rune, 0)
+		row := make([]*Node, 0)
 		// Day-specific code
 		for colIndex, char := range line {
-			row = append(row, char)
+			node := &Node{x: colIndex, y: len(grid), content: char, prev: make([]*PriorityQueueItem, 0)}
+			row = append(row, node)
 			if char == 'S' {
-				startPosition = Position{x: colIndex, y: len(grid), direction: 90} // start facing east
+				startPosition = node
 			} else if char == 'E' {
-				endPosition = Position{x: colIndex, y: len(grid)}
+				endPosition = node
 			}
 		}
 		grid = append(grid, row)
@@ -228,9 +256,10 @@ func part1() {
 	pq := make(PriorityQueue, 0) // priority queue makes dijkstra much easier
 	heap.Init(&pq)
 	// Add the start node
-	item := &Item{
-		position: startPosition,
-		priority: 0,
+	item := &PriorityQueueItem{
+		node:      startPosition,
+		direction: 90, // the start direction is east
+		priority:  0,
 	}
 	heap.Push(&pq, item)
 	visited := make(map[string]bool)
@@ -240,9 +269,4 @@ func part1() {
 	}
 	printGrid(grid, path)
 	fmt.Println("The final result is: ", result)
-}
-
-func part2() {
-	// https://adventofcode.com/2024/day/16#part2
-	//
 }
